ad9cb6f39825a827683022e71bcd90280f3a8e35
Ahmad Raza <95644785+Ahmadraza1394@users.noreply.github.com>
2024-07-14 00:38:53 +0500

    final

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        orange: {
+          500: "#FFA500", // customize to match the exact shade of orange
+        },
+      },
+      fontFamily: {
+        sans: ["YourDesiredFont", "sans-serif"], // customize to match the font
+      },
+    },
+  },
+};
